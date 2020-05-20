1a493296646935c50a9cfa1c7f499fa6d7257fe1
jeanPcr <jean.coding971@gmail.com>
2020-05-20 18:12:49 -0400

    refactoring dist folder

--- dist/index.js
+++ dist/index.js
@@ -2,9 +2,6 @@ import Game from "./Game.js";
 
 let canvas = document.querySelector("#gameScreen");
 let ctx = canvas.getContext("2d");
-let arrowLeft = document.querySelector(".img_arrowLeft");
-let arrowRight = document.querySelector(".img_arrowRight");
-console.log(arrows);
 
 let GAME_HEIGHT = window.innerHeight - 50;
 let GAME_WIDTH = window.innerWidth - 100;
@@ -20,14 +17,6 @@ if (window.innerWidth < 1000 && window.innerHeight > 700) {
 
 canvas.setAttribute("width", GAME_WIDTH);
 canvas.setAttribute("height", GAME_HEIGHT);
-let player_name = "";
-
-let input = document.querySelector(".player_name");
-
-input.addEventListener("input", (e) => {
-  player_name = e.target.value;
-});
-console.log(player_name);
 
 let game = new Game(GAME_WIDTH, GAME_HEIGHT);
 let lastTime = 0;
